db033c2e3e77b3a9c84c17b80ce12275a56f392e
cocoL123 <1715285528@qq.com>
2018-07-06 19:57:10 +0800

    first update

--- Scripts/test_01.py
+++ Scripts/test_01.py
@@ -1,9 +1,14 @@
 # __author: Honorbaby
 # date: 2018/7/6
+import pytest,allure
 
 
 class Test_01:
-
+    @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
     def test_01_1(self):
-
         assert 0
+
+    @allure.step('第一步')
+    def test_01_1(self):
+        allure.attach('描述', "我是第一步的描述")
+        assert 1
